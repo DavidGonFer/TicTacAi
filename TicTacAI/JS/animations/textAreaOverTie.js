ada7e61b3d90f79dea7367c797ca6b1a126db217
David González Fernández <102176150+DavidGonFer@users.noreply.github.com>
2023-05-18 12:13:49 +0200

    Subir el proyecto

---
+++ TicTacAI/JS/animations/textAreaOverTie.js
@@ -0,0 +1,63 @@
+function textAreaOverTie() {
+    // Crear el nuevo div
+    var overlayDiv = document.createElement('div');
+    overlayDiv.id = 'overlayDiv';
+
+    // Establecer estilos CSS para el div
+    overlayDiv.style.position = 'fixed';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.width = '100%';
+    overlayDiv.style.height = '100%';
+    overlayDiv.style.background = 'rgba(0, 0, 0, 0.5)'; // Fondo semitransparente
+    overlayDiv.style.zIndex = '9999'; // Asegurarse de que el div esté por encima de todo
+    overlayDiv.style.opacity = '0'; // Establecer la opacidad inicial en 0 para que sea invisible
+
+    // Agregar el div al cuerpo del documento
+    document.body.appendChild(overlayDiv);
+
+    // Centrar el div
+    overlayDiv.style.display = 'flex';
+    overlayDiv.style.alignItems = 'center';
+    overlayDiv.style.justifyContent = 'center';
+
+    // Animación de entrada suave
+    overlayDiv.style.animation = 'fadeIn 1s forwards';
+
+    // Código adicional para añadir contenido al div
+    var contentDiv = document.createElement('div');
+    contentDiv.id = 'contentDivTie';
+    if (document.body.classList.contains("dark")) {
+        contentDiv.style.backgroundColor = '#333642';
+        contentDiv.style.color ='white';
+      }else{
+        contentDiv.style.backgroundColor = 'white';
+        contentDiv.style.color ='black';
+      }
+      
+    var contentText = document.createElement('p');
+    contentText.innerHTML = 'Vaya, parece que habeis empatado tu y la IA... Estuviste a punto de ganar¿Por que no vuelves a probar?<br>';
+    contentDiv.appendChild(contentText);
+
+    // Crear el botón verde
+    var button = document.createElement('button');
+    button.classList.add('btn', 'btn-success');
+    button.textContent = 'Volver a jugar';
+
+ 
+
+    // Agregar un controlador de eventos al botón
+    button.addEventListener('click', function () {
+        // Obtener el estado actual del modo oscuro
+        const isDarkMode = document.body.classList.contains("dark");
+
+        // Guardar el estado del modo oscuro en el almacenamiento local
+        localStorage.setItem("darkMode", isDarkMode);
+        location.reload(); // Recargar la página
+    });
+
+    // Añadir el contenido al div superpuesto
+    contentDiv.appendChild(button);
+    overlayDiv.appendChild(contentDiv);
+
+}
